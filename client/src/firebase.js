212027f74432e487746db0904807180e5b88aa3d
Ishaanapte0108 <ishaanmapte@gmail.com>
2024-06-14 18:54:24 +0530

    commit 11

---
+++ client/src/firebase.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATPaD6regKBtPwnKT4EoCLeQOnGXnw-iI",
+  authDomain: "continuume-7ae06.firebaseapp.com",
+  projectId: "continuume-7ae06",
+  storageBucket: "continuume-7ae06.appspot.com",
+  messagingSenderId: "265333556636",
+  appId: "1:265333556636:web:41f723698a7496ada4f459",
+  measurementId: "G-KHD282020N"
+};
+
+
+export const app = initializeApp(firebaseConfig);
