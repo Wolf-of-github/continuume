c53a73a9040e3d471c444a11b5c7aee88289171f
Ishaanapte0108 <ishaanmapte@gmail.com>
2024-07-28 01:25:32 +0530

    update index footer and hoem

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,8 +7,7 @@ const Footer = () => {
     <footer className="text-gray-700 body-font">
     <div className="bg-gray-200">
       <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-        <p className="text-gray-500 text-sm text-center sm:text-left">© 2024 Continuume Consultancy Services - Ledge
-          {/* <a href="https://twitter.com/knyttneve" className="text-gray-600 ml-1" target="_blank" rel="noopener noreferrer">@knyttneve</a> */}
+        <p className="text-gray-500 text-sm text-center sm:text-left">© 2024 Ledge -  Continuume Consultancy Services
         </p>
         <span className="sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-500 text-sm"> Excel beyond borders</span>
       </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -36,7 +36,7 @@ const Home = () => {
               </div>
               
               <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                <img className="object-cover object-center rounded" alt="hero" src="../../public/images/travelWorld.jpg" />
+                <img className="object-cover object-center rounded" alt="hero" src="/images/travelWorld.jpg" />
               </div>
             </div>
           </section>
