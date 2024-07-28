b74f2e072116b2a562eea4d1a7b49b44c3c36f4f
Ishaanapte0108 <ishaanmapte@gmail.com>
2024-07-28 15:16:44 +0530

    logo update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -13,9 +13,10 @@ export default function Header() {
   return (
     <nav className="bg-white border-b border-gray-200">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        
         <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap text-black">Ledge</span>
+          <img src="/images/logo.png" className="h-8" alt="Flowbite Logo" />
+          {/* <span className="self-center text-2xl font-semibold whitespace-nowrap text-black">Ledge</span> */}
         </Link>
         
         <button
