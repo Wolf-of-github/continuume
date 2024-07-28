51ffa7f92a1619dee81854274d2102ca02f544ed
Ishaanapte0108 <ishaanmapte@gmail.com>
2024-07-28 15:19:45 +0530

    udpate bg color of console boxes

--- client/src/pages/Console.jsx
+++ client/src/pages/Console.jsx
@@ -40,7 +40,7 @@ export default function Console() {
         <div className="flex flex-wrap -m-4">
           {options.map((option, index) => (
             <div className="p-4 md:w-1/3" key={index}>
-              <div className="flex rounded-lg h-full bg-gray-200 p-8 flex-col shadow-md hover:shadow-lg transition-shadow duration-200">
+              <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col shadow-md hover:shadow-lg transition-shadow duration-200">
                 <div className="flex items-center mb-3">
                   <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
                     {option.icon}
