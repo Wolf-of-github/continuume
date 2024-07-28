7a4552d0e24e5b3e506624d1512d72ad079e29ae
Ishaanapte0108 <ishaanmapte@gmail.com>
2024-07-28 14:49:27 +0530

    make events page responsive

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -11,7 +11,7 @@ export default function Events() {
   const clearSelectedEvent = () => {
     setSelectedEvent(null); // Clear selected event details
   };
-  
+
   const getFormattedDate = (date) => {
     const originalDate = new Date(date);
     const options = { month: 'short', day: 'numeric', year: 'numeric' };
@@ -20,54 +20,48 @@ export default function Events() {
   };
 
   return (
-    <div className="px-2 py-4 sm:px-4">
+    <div className="bg-white min-h-screen px-4 py-6">
       <div className="flex items-center justify-between pb-6">
         <div>
-          <h2 className="font-bold text-xl text-gray-700">View Events</h2>
+          <h2 className="font-bold text-2xl text-gray-900">View Events</h2>
           <span className="text-base text-gray-500">
             Please follow the events calendar to complete your application on time
           </span>
         </div>
       </div>
 
-      <div className="grid grid-cols-5 gap-4">
-        <div className="col-span-3">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+        <div className="lg:col-span-2">
           <Calendar onEventClick={handleEventClick} />
         </div>
-        <div className="col-span-2 overflow-auto">
+        <div className="lg:col-span-1 overflow-auto">
           {selectedEvent ? (
-            
-            <div className='rounded-lg p-4 border'>
-              { selectedEvent.eventBannerImage && (
+            <div className="rounded-lg p-4 border bg-gray-100 shadow-md">
+              {selectedEvent.eventBannerImage && (
                 <div className="mb-4">
                   <img src={selectedEvent.eventBannerImage} alt="Event Banner" className="w-full h-48 object-contain rounded-lg" />
                 </div>
-                )
-              }
+              )}
 
-              <div className="grid grid-cols-2 gap-4 mb-2">
-
-                {/* Event name */}
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-2">
                 <div>
                   <label className="block text-gray-700 text-sm font-bold mb-2">
                     Event Name
                   </label>
                   <div className="appearance-none border rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-                    {selectedEvent.eventName} 
+                    {selectedEvent.eventName}
                   </div>
                 </div>
 
-                {/* Event Date */}
                 <div>
                   <label className="block text-gray-700 text-sm font-bold mb-2">
                     Event Date
                   </label>
                   <div className="appearance-none border rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-                    {getFormattedDate(selectedEvent.eventDate)} 
+                    {getFormattedDate(selectedEvent.eventDate)}
                   </div>
                 </div>
 
-                {/* Event Time */}
                 <div>
                   <label className="block text-gray-700 text-sm font-bold mb-2">
                     Event Time
@@ -77,7 +71,6 @@ export default function Events() {
                   </div>
                 </div>
 
-                {/* Event Venue */}
                 <div>
                   <label className="block text-gray-700 text-sm font-bold mb-2">
                     Event Venue
@@ -86,11 +79,9 @@ export default function Events() {
                     {selectedEvent.eventVenue}
                   </div>
                 </div>
-
               </div>
 
-              {/* Event Description */}
-              <div className='mb-2'>
+              <div className="mb-2">
                 <label className="block text-gray-700 text-sm font-bold mb-2">
                   Event Description
                 </label>
@@ -99,18 +90,18 @@ export default function Events() {
                 </div>
               </div>
 
-              {/* Registration Link */}
-              <div className='mb-2'>
+              <div className="mb-2">
                 <label className="block text-gray-700 text-sm font-bold mb-2">
                   Registration Link
                 </label>
                 <div className="appearance-none border rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-                  <a href={selectedEvent.eventRegistrationLink} target='_blank' rel="noopener noreferrer">{selectedEvent.eventRegistrationLink}</a>
+                  <a href={selectedEvent.eventRegistrationLink} target="_blank" rel="noopener noreferrer">
+                    {selectedEvent.eventRegistrationLink}
+                  </a>
                 </div>
               </div>
 
-              {/* Event Resources */}
-              <div className=''>
+              <div className="">
                 {selectedEvent.eventResources.length > 0 && (
                   <div>
                     <label className="block text-gray-700 text-sm font-bold mb-2">
@@ -118,7 +109,12 @@ export default function Events() {
                     </label>
                     {selectedEvent.eventResources.map((resource, index) => (
                       <div key={index} className="flex space-x-4 py-2">
-                        <a href={resource} target='_blank' rel="noopener noreferrer" className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                        <a
+                          href={resource}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="bg-indigo-500 hover:bg-indigo-700 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        >
                           View Resource {index + 1}
                         </a>
                       </div>
@@ -126,11 +122,11 @@ export default function Events() {
                   </div>
                 )}
               </div>
-
             </div>
           ) : (
-            <div className='h-screen rounded-lg border p-4 pb-0 text-center flex items-center justify-center'>
-              Select an event to view details</div>
+            <div className="h-48 rounded-lg border p-4 text-center flex items-center justify-center bg-gray-100">
+              Select an event to view details
+            </div>
           )}
         </div>
       </div>
